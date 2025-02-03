cae2adc0475bf078aeced26878002d553ddb3dff
Abdelrahman Nassar <aanassar.io@gmail.com>
2025-02-03 14:59:48 +0200

    finish carousel section

---
+++ app/_components/UI/Carousel/Carousel.jsx
@@ -0,0 +1,71 @@
+"use client";
+
+import React, { useState } from "react";
+import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
+import { BiSolidLeftArrow , BiSolidRightArrow } from "react-icons/bi";
+import Image from "next/image";
+import Link from "next/link";
+
+import "./Carousel.scss";
+
+const Carousel = ({ items }) => {
+  const [slide, setSlide] = useState(0);
+
+  const prevSlide = () => {
+    setSlide(slide === items.length - 1 ? 0 : slide + 1);
+  };
+
+  const nextSlide = () => {
+    setSlide(slide === 0 ? items.length - 1 : slide - 1);
+  };
+
+  return (
+    <div className="Carousel">
+      <div style={{ overflowX: "hidden" }}>
+        <div
+          className="container"
+          style={{ transform: `translateX(${slide * 100}%)` }}
+        >
+          {items.map(({ image, text }, idx) => {
+            return (
+              <div
+                className={`carousel-item ${
+                  slide === idx ? "slide" : "slide slide-hidden"
+                }`}
+                key={idx}
+              >
+                <div className="image-container">
+                  <Image src={image.url} alt="course-image" placeholder="blur" />
+                </div>
+                <div className="text-container">
+                  <Link href={text.link}>
+                    <h3>{text.heading}</h3>
+                    <p>{text.description}</p>
+                    <span className="price">{text.price} جنيه</span>
+                  </Link>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+      <BiSolidRightArrow onClick={nextSlide} className="arrow arrow-right" />
+      <BiSolidLeftArrow onClick={prevSlide} className="arrow arrow-left" />
+      <span className="indicators">
+        {items.map((_, idx) => {
+          return (
+            <button
+              key={idx}
+              className={
+                slide === idx ? "indicator indicator-active" : "indicator indicator-inactive"
+              }
+              onClick={() => setSlide(idx)}
+            ></button>
+          );
+        })}
+      </span>
+    </div>
+  );
+};
+
+export default Carousel;
---
+++ app/_components/layout/FeaturedCoursesSection/FeaturedCoursesSection.jsx
@@ -0,0 +1,88 @@
+import React from "react";
+
+import "./FeaturedCoursesSection.scss";
+
+import course1 from "@/public/assets/images/courses/course-1.jpg";
+import course2 from "@/public/assets/images/courses/course-2.jpg";
+import course3 from "@/public/assets/images/courses/course-3.jpg";
+import course4 from "@/public/assets/images/courses/course-4.jpg";
+
+import Carousel from "../../UI/Carousel/Carousel";
+
+
+import Image from "next/image";
+
+const items = [
+  {
+    image: {
+      url: course1,
+      alt: "course",
+    },
+    text: {
+      link: "/courses",
+      heading: "كورس البيولوجيا الجزيئية كاملاً للعام والأزهر",
+      description:"أهلا يك صديقى المراكم ية هنفرم كل اللى فات بإذن الله مى أقل من أسبوعين بس محتاجين نشد جامد ونعمل كل اللى علينا عشا ربنا يكرمنا",
+      price: "200"
+    }
+  },
+  {
+    image: {
+      url: course2,
+      alt: "course",
+    },
+    text: {
+      link: "/courses",
+      heading: "كورس البيولوجيا الجزيئية كاملاً للعام والأزهر",
+      description:"أهلا يك صديقى المراكم ية هنفرم كل اللى فات بإذن الله مى أقل من أسبوعين بس محتاجين نشد جامد ونعمل كل اللى علينا عشا ربنا يكرمنا",
+      price: "150"
+    }
+  },
+  {
+    image: {
+      url: course3,
+      alt: "course",
+    },
+    text: {
+      link: "/courses",
+      heading: "كورس البيولوجيا الجزيئية كاملاً للعام والأزهر",
+      description:"أهلا يك صديقى المراكم ية هنفرم كل اللى فات بإذن الله مى أقل من أسبوعين بس محتاجين نشد جامد ونعمل كل اللى علينا عشا ربنا يكرمنا",
+      price: "150"
+    }
+  },
+  {
+    image: {
+      url: course4,
+      alt: "course",
+    },
+    text: {
+      link: "/courses",
+      heading: "كورس البيولوجيا الجزيئية كاملاً للعام والأزهر",
+      description:"أهلا يك صديقى المراكم ية هنفرم كل اللى فات بإذن الله مى أقل من أسبوعين بس محتاجين نشد جامد ونعمل كل اللى علينا عشا ربنا يكرمنا",
+      price: "150"
+    }
+  },
+  
+];
+
+const FeaturedCoursesSection = () => {
+  return (
+    <>
+      <section className="FeaturedCoursesSection">
+        <div className="text-container">
+          <h1 className="main-heading">
+            الكورسات <span className="colored">المُقترحة</span>
+          </h1>
+          <p className="description">
+            استعد للتفوق مع أفضل الكورسات على منصة م. محمد هيكل! الكورسات دي مش
+            بس هتسهل عليك المذاكرة، لكنها كمان هتخليك مميز بين زمايلك!
+          </p>
+        </div>
+        <div className="carousel-container">
+          <Carousel items={items}></Carousel>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default FeaturedCoursesSection;
